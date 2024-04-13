7c1066ab348de137570a1e17d29bf50ec05b209c
ShaunSDavid <shaunsylvaindavid@gmail.com>
2024-04-13 16:13:11 +0530

    Backend intialised

---
+++ canteenmanagement/src/main/java/com/canteen/canteenmanagement/CanteenmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.canteen.canteenmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanteenmanagementApplication.class, args);
+	}
+
+}
